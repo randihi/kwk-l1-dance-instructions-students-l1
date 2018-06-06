179e19f7ba4441e0abed6011bf46b232e49784e4
randihi <randihigashi@gmail.com>
2018-06-06 04:35:34 +0000

    Done.

---
+++ about_me.rb
@@ -0,0 +1,10 @@
+local_greeting = "Hey dude"
+your_name = "Chen" 
+my_name = "Randi"
+time_of_day = "summer time"
+
+def greeting
+  puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{time_of_day}?"
+end
+
+greeting
