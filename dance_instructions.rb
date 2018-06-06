b7f378b6ac4a14633c8458e6b9b6db429acab54c
randihi <randihigashi@gmail.com>
2018-06-06 04:35:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -28,6 +28,7 @@ def lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
   puts "Lift right arm up and point right elbow diagonally to the right."
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+  puts "Do this 7 more times"
 end
 
 def bust_a_move
