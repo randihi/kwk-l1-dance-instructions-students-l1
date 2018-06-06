518c349e63e8f74dfbf7ae3fdbd69e68d58bd7a9
randihi <randihigashi@gmail.com>
2018-06-06 04:29:30 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,74 @@
 
 # Code your methods below
+
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a smalll skip-step backward"
+end
+
+def skip_step
+  puts "Lower left foor to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"  
+end
+
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+end
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+end
+
+def bust_a_move
+  puts "Twirl around in a circle"
+end
+
+starting_stance
+bust_a_move
+base_footwork
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+bob_the_reins
+skip_step
+lasso
+skip_step
+lasso
+skip_step
+lasso
+skip_step
+lasso
+skip_step
+lasso
+skip_step
+lasso
+skip_step
+lasso
+skip_step
+
