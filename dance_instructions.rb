fea19450b73fc0636b9185fc2c718a7d07e9e64b
randihi <randihigashi@gmail.com>
2018-06-06 04:31:07 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -11,13 +11,10 @@ def base_footwork
   skip_step("left foot")
 end
 
-def skip_step
-  puts "Lower left foor to the ground"
-  puts "Bounce left foot back up slightly, kicking it a few inches back"
-  puts "Left, Right, Left and Left"
-  puts "Lower right foot to the ground"
-  puts "Bounce right foot back up slightly, kicking it a few inches back"
-  puts "Right, Left, Right and Right"  
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
+  puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end
 
 def bob_the_reins
